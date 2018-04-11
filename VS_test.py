f38fa5d5e8739dcde385cef2d3d2bfd62d09b23a
DESKTOP-UT010R7\aaron <aaron.d.jennings@gmail.com>
2018-04-11 09:59:37 -0400

    vs_test

--- VS_test.py
+++ VS_test.py
@@ -1,7 +1,3 @@
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
-
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
-plt.show()
+x = [1, 2, 3, 4, 4,]
+x.pop(0)
+print(x)
